f0c8368507b3206aff323b2cc5b832d0a7e2aa40
ogoodocity <ogodochibuike@gmail.com>
2021-05-05 11:38:09 +0100

    fickka updated

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'fickkadb',
+        'USER': 'root',
+        'PASSWORD': 'nigeria111',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
-
 
 
 # Password validation
